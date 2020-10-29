71324d454a376bd59634127925ae3994584c3b49
Chad Cox <chadcox@Chads-MacBook-Pro.local>
2020-10-29 12:17:38 -0600

    mvp

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,11 @@
+import { useLocalStorage } from './useLocalStorage';
+
+export const useDarkMode = () => {
+    
+    const [someValue, setSomeValue] = useLocalStorage('key', false)
+    const toggleDarkMode = () => {
+        setSomeValue(!someValue)
+}
+
+return [someValue, toggleDarkMode]
+}
